07ff86c2115fa2987686bf54eb8950e4e3576c23
songhee24 <akaratel61@gmail.com>
2022-08-22 20:21:47 +0600

    changed port to proxy traffic

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = 3000
 
 const swaggerUi = require('swagger-ui-express'),
 swaggerDocument = require('./swagger.json');
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App running on port ${process.env.PORT || 3000}.`)
+})
 
 app.use(bodyParser.json())
 app.use(
@@ -29,7 +35,3 @@ app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
-
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
