eb78c338092642609966b65727562c1cf6a84276
songhee24 <akaratel61@gmail.com>
2022-08-22 19:19:37 +0600

    configure heroku deploy

--- queries.js
+++ queries.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 const getUsers = (request, response) => {
