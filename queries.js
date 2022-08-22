3d23d06bba26567077989f70b668fbc0615ed883
songhee24 <akaratel61@gmail.com>
2022-08-22 20:03:11 +0600

    db uri changed

--- queries.js
+++ queries.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const Pool = require('pg').Pool
 
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const connectionString = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
     connectionString: connectionString,
