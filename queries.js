4f54abc59b527e5812e4b310ad68075785b6a7f8
songhee24 <akaratel61@gmail.com>
2022-08-23 16:29:24 +0600

    trying to change database connection

--- queries.js
+++ queries.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 const Pool = require('pg').Pool
-
-const connectionString = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-
 const pool = new Pool({
-    connectionString,
+    connectionString: process.env.DATABASE_URL,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
     ssl: {
         rejectUnauthorized: false
     }
